65a6a1ec77c93f89c0e0e60a90c21cc9c614ff55
AngieProg <angie_db@hotmail.com>
2023-10-20 14:28:30 -0600

    Add delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -34,9 +34,31 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
+        deleteUserStart: (state) => {
+            state.loading = true;
+          },
+          deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+          },
+          deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
     }
 });
 
-export const {signInSuccess, signInFailure, signInStart, updateUserFailure, updateUserSuccess, updateUserStart} = userSlice.actions;
+export const {
+    signInSuccess, 
+    signInFailure, 
+    signInStart, 
+    updateUserFailure, 
+    updateUserSuccess, 
+    updateUserStart,
+    deleteUserFailure,
+    deleteUserSuccess,
+    deleteUserStart,
+    } = userSlice.actions;
 
 export default userSlice.reducer;
